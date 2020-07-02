275572a7ab90d5a51b75a95526efb6b64fab20b4
czongwu <czongwu1@163.com>
2020-07-02 17:28:24 +0800

    Initial commit

---
+++ Function.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from PyQt5.QtWidgets import QMainWindow
+from view import Ui_MainWindow
+
+class function(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+
---
+++ uploader.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import sys
+
+from PyQt5.QtCore import QTranslator
+from PyQt5.QtWidgets import QApplication
+from Function import function
+
+class show_func(function):
+    def __init__(self):
+        function.__init__(self)
+        self.setWindowTitle("NC-UpLoader")
+
+def translate():
+    translateLoad = QTranslator()
+    translateLoad.load('./res/qt_zh_CN.qm')
+    return translateLoad
+
+if __name__ == '__main__':
+    app = QApplication([])
+    translateInstall = translate()
+    app.installTranslator(translateInstall)
+    win = show_func()
+    win.show()
+    sys.exit(app.exec_())
